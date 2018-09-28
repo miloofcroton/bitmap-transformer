eaa434c0f4449f9d0db2bf72c6db2c56759407e0
Jack <ajackofsometrades@gmail.com>
2018-09-28 12:59:00 -0700

    using unsigned ints!

--- test/pixel-reader.test.js
+++ test/pixel-reader.test.js
@@ -20,9 +20,9 @@ describe('Pixel Reader', () => {
 
         reader.on('end', () => {
 
-            assert.deepEqual(colors[0], { offset: 0, b: 116, g: 63, r: 63 });
-            assert.deepEqual(colors[1], { offset: 24, b: 116, g: 63, r: 63 });
-            assert.deepEqual(colors[2], { offset: 48, b: 116, g: 63, r: 63 });
+            assert.deepEqual(colors[0], { offset: 0, b: 220, g: 63, r: 63 });
+            assert.deepEqual(colors[1], { offset: 24, b: 220, g: 63, r: 63 });
+            assert.deepEqual(colors[2], { offset: 48, b: 220, g: 63, r: 63 });
             done();
 
         });
@@ -30,9 +30,9 @@ describe('Pixel Reader', () => {
         const buffer = Buffer.alloc(24 * 3); 
 
         for(let i = 0; i < 3; i++) {
-            buffer.writeInt8(116, i * 24);
-            buffer.writeInt8(63, i * 24 + 8);
-            buffer.writeInt8(63, i * 24 + 16);
+            buffer.writeUInt8(220, i * 24);
+            buffer.writeUInt8(63, i * 24 + 8);
+            buffer.writeUInt8(63, i * 24 + 16);
         }
 
         reader.read(buffer);
