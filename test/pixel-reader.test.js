e2e65937c068f37f8cb3e4c934c6ca9096d2c2c0
Jack <ajackofsometrades@gmail.com>
2018-09-28 11:28:23 -0700

    clean up

--- test/pixel-reader.test.js
+++ test/pixel-reader.test.js
@@ -20,8 +20,6 @@ describe('Pixel Reader', () => {
 
         reader.on('end', () => {
 
-            console.log(colors);
-
             assert.deepEqual(colors[0], { offset: 0, b: 116, g: 63, r: 63 });
             assert.deepEqual(colors[1], { offset: 24, b: 116, g: 63, r: 63 });
             assert.deepEqual(colors[2], { offset: 48, b: 116, g: 63, r: 63 });
@@ -29,24 +27,14 @@ describe('Pixel Reader', () => {
 
         });
 
-        const buffer = Buffer.alloc(24 * 3); // for three pixels, 24 bits * 3 = 72 bits
+        const buffer = Buffer.alloc(24 * 3); 
 
-        // pixel #1
-        buffer.writeInt8(116, 0);
-        buffer.writeInt8(63, 8);
-        buffer.writeInt8(63, 16);
+        for(let i = 0; i < 3; i++) {
+            buffer.writeInt8(116, i * 24);
+            buffer.writeInt8(63, i * 24 + 8);
+            buffer.writeInt8(63, i * 24 + 16);
+        }
 
-        // pixel #2
-        buffer.writeInt8(116, 24);
-        buffer.writeInt8(63,32);
-        buffer.writeInt8(63, 40);
-
-        // pixel #3
-        buffer.writeInt8(116, 48);
-        buffer.writeInt8(63, 56);
-        buffer.writeInt8(63, 64);
-
-        // Call read method with your buffer
         reader.read(buffer);
     });
 
