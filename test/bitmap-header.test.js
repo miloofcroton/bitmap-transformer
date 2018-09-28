bea49b438868e7ceb2049c54f572c0ebc96d22b5
Jack <ajackofsometrades@gmail.com>
2018-09-27 18:57:28 -0700

    green tests for bitmap header!

--- test/bitmap-header.test.js
+++ test/bitmap-header.test.js
@@ -2,17 +2,22 @@ const assert = require('assert');
 const constants = require('../lib/bitmap-constants');
 const BitmapHeader = require('../lib/bitmap-header');
 
+const fs = require('fs');
+
 describe('bitmap header', () => {
 
     let buffer = null;
-    beforeEach(() => {
+    beforeEach(done => {
         // TODO: file read './test/test-bitmap.bmp' and put the promise return into buffer variable
+        fs.readFile('./test/test-bitmap.bmp', (err, data) => {
+            if(err) return done(err);
+            console.log(data);
+            buffer = data;
+            done();
+        });
     });
 
     it('has correct specs', () => {
-        // TODO: read the wiki spec docs to figure out what these values should be.
-        // You don't need to change this test, you need to put the correct values into
-        // '../lib/bitmap-constants'
         assert.ok(constants.PIXEL_OFFSET);
         assert.ok(constants.BITS_PER_PIXEL_OFFSET);
         assert.ok(constants.FILE_SIZE_OFFSET);
@@ -23,6 +28,7 @@ describe('bitmap header', () => {
         // on the BitmapHeader in its constructor.
         // These test values are correct for the supplied test-bitmap.bmp
         const header = new BitmapHeader(buffer);
+
         assert.equal(header.pixelOffset, 54);
         assert.equal(header.bitsPerPixel, 24);
         assert.equal(header.fileSize, 30054);
