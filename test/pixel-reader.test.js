c4171f854ec7fa08224dc4cb117a893b21d4550d
Jack <ajackofsometrades@gmail.com>
2018-09-29 16:02:08 -0700

    cleanup

--- test/pixel-reader.test.js
+++ test/pixel-reader.test.js
@@ -7,34 +7,18 @@ describe('Pixel Reader', () => {
         const reader = new PixelReader({ bitsPerPixel: 24 });
         const colors = [];
 
-        reader.on('color', color => {
-            const { offset, b, g, r } = color;
-            colors.push({
-                offset: offset,
-                b: b,
-                g: g,
-                r: r
-            });
-        });
+        reader.on('color', color => colors.push(color));
 
         reader.on('end', () => {
-            assert.deepEqual(colors[0], { offset: 0, b: 220, g: 63, r: 63 });
-            assert.deepEqual(colors[1], { offset: 3, b: 220, g: 63, r: 63 });
-            assert.deepEqual(colors[2], { offset: 6, b: 220, g: 63, r: 63 });
+            assert.deepEqual(colors[0], { offset: 0, b: 0, g: 28, r: 56 });
+            assert.deepEqual(colors[1], { offset: 3, b: 84, g: 112, r: 140 });
+            assert.deepEqual(colors[2], { offset: 6, b: 168, g: 196, r: 224 });
             done();
         });
 
         const buffer = Buffer.alloc(24 * 3);
-
-        buffer.writeUInt8(220, 0);
-        buffer.writeUInt8(63, 1);
-        buffer.writeUInt8(63, 2);
-        buffer.writeUInt8(220, 3);
-        buffer.writeUInt8(63, 4);
-        buffer.writeUInt8(63, 5);
-        buffer.writeUInt8(220, 6);
-        buffer.writeUInt8(63, 7);
-        buffer.writeUInt8(63, 8);
+        for(let i = 0; i < 9; i++) buffer.writeUInt8(28 * i, i);
+        
         reader.read(buffer);
     });
 
