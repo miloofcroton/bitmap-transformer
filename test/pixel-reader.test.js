59961278402ffbe9514a24b484bc4a28e1985d92
Jack <ajackofsometrades@gmail.com>
2018-09-29 14:05:15 -0700

    pixel reader fixed... oy vey

--- test/pixel-reader.test.js
+++ test/pixel-reader.test.js
@@ -5,7 +5,6 @@ describe('Pixel Reader', () => {
 
     it('reads pixel from buffer', done => {
         const reader = new PixelReader({ bitsPerPixel: 24 });
-        
         const colors = [];
 
         reader.on('color', color => {
@@ -19,23 +18,25 @@ describe('Pixel Reader', () => {
         });
 
         reader.on('end', () => {
-
             assert.deepEqual(colors[0], { offset: 0, b: 220, g: 63, r: 63 });
-            assert.deepEqual(colors[1], { offset: 24, b: 220, g: 63, r: 63 });
-            assert.deepEqual(colors[2], { offset: 48, b: 220, g: 63, r: 63 });
+            assert.deepEqual(colors[1], { offset: 3, b: 220, g: 63, r: 63 });
+            assert.deepEqual(colors[2], { offset: 6, b: 220, g: 63, r: 63 });
             done();
-
         });
 
-        const buffer = Buffer.alloc(24 * 3); 
+        const buffer = Buffer.alloc(24 * 3);
 
-        for(let i = 0; i < 3; i++) {
-            buffer.writeUInt8(220, i * 24);
-            buffer.writeUInt8(63, i * 24 + 8);
-            buffer.writeUInt8(63, i * 24 + 16);
-        }
-
+        buffer.writeUInt8(220, 0);
+        buffer.writeUInt8(63, 1);
+        buffer.writeUInt8(63, 2);
+        buffer.writeUInt8(220, 3);
+        buffer.writeUInt8(63, 4);
+        buffer.writeUInt8(63, 5);
+        buffer.writeUInt8(220, 6);
+        buffer.writeUInt8(63, 7);
+        buffer.writeUInt8(63, 8);
         reader.read(buffer);
     });
 
 });
+
