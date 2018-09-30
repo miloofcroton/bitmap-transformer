7a4734acbccac554f8cc1d984cdebf8544ab4b13
Jack <ajackofsometrades@gmail.com>
2018-09-29 17:50:10 -0700

    confirming table size checking works

--- test/bitmap-header.test.js
+++ test/bitmap-header.test.js
@@ -23,8 +23,28 @@ describe('bitmap header', () => {
 
     it('parses header data', () => {
         const header = new BitmapHeader(buffer);
-        assert.equal(header.pixelOffset, 54);
         assert.equal(header.bitsPerPixel, 24);
+        assert.equal(header.pixelOffset, 54);
         assert.equal(header.fileSize, 30054);
+    });
+
+});
+
+describe('checks color table', () => {
+
+    let buffer = null;
+
+    beforeEach(done => {
+        fs.readFile('./test/pics/palette-test-bitmap.bmp', (err, data) => {
+            if(err) return done(err);
+            buffer = data;
+            done();
+        });
+    });
+
+    it('reads table data', () => {
+        const header = new BitmapHeader(buffer);
+        assert.equal(header.paletteSize, 256);
+        assert.equal(header.pixelOffset, 1078);
     });
 });
